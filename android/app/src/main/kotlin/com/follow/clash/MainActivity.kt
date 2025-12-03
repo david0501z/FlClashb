948563faa9b2e8029f297a66fcbb2a2a92779738
david0501z <zhangdw0501@outlook.com>
2025-12-03 12:26:44 +0800

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/follow/clash/MainActivity.kt
+++ android/app/src/main/kotlin/com/follow/clash/MainActivity.kt
@@ -6,6 +6,7 @@ import com.follow.clash.common.GlobalState
 import com.follow.clash.plugins.AppPlugin
 import com.follow.clash.plugins.ServicePlugin
 import com.follow.clash.plugins.TilePlugin
+import com.follow.clash.plugins.WebViewProxyPlugin
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import kotlinx.coroutines.CoroutineScope
